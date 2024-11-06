0c6c39ced1a1cfa3c740ec296cbc17babd057604
Muhamad Fachri <mfachri271004@gmail.com>
2024-11-06 21:23:18 +0700

    update configs

--- configs/config.go
+++ configs/config.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=fachri123 dbname=db-ecommerce port=5432 sslmode=disable"
+	dsn := "host=127.0.0.1 user=postgres password=fachri123 dbname=db-ecommerce port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
