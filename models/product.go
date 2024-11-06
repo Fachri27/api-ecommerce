746b8e71a4ef33f550f84ed81c34c77916573e1e
Muhamad Fachri <mfachri271004@gmail.com>
2024-11-06 20:59:13 +0700

    update model product

--- models/product.go
+++ models/product.go
@@ -8,9 +8,10 @@ import (
 type Product struct {
 	gorm.Model
 	Id           uint    `gorm:"primary_key" json:"id"`
-	Product_name string  `json:"product_name"`
+	Product_name string  `json:"product_name" binding:"required"`
 	Price        float64 `json:"price"`
-	Deskripsi    string  `json:"deskripsi"`
+	Deskripsi    string  `json:"deskripsi" binding:"required"`
 	Stock        int     `json:"stock"`
 	Category_id  uint    `json:"category_id"`
+	Image_id     *uint   `json:"image_id"`
 }
